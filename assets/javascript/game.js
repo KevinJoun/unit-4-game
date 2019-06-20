f12eb7b34e4ab694d4f32d1e49472f07303e5daf
SilverBell17 <kevinjoun17@gmail.com>
2019-06-19 17:50:43 -0700

    added to game.js. random numbers are generated for crystals. Still have to assign the random numbers to the crystal.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,26 @@
 $(document).ready(function(){
-var randomNumber;
+var randomTargetNumber;
+var randomJewelNumber1;
+var randomJewelNumber2;
+var randomJewelNumber3;
+var randomJewelNumber4;
 // this function gets a random number between 19 and 120.
 function randomTargetNumber(){
     return Math.floor(Math.random() * 102) + 19;
 }
 function randomJewelNumber(){
-    
+    return Math.floor(Math.random() * 12) + 1;
 }
-randomNumber = randomTargetNumber(); // this runs the function random() and returns the output of the function as the variable randomNumber
-console.log(randomNumber); //logs the randomNumber
-$("#targetNumber").html(randomNumber);
+randomJewelNumber1 = randomJewelNumber();
+randomJewelNumber2 = randomJewelNumber();
+randomJewelNumber3 = randomJewelNumber();
+randomJewelNumber4 = randomJewelNumber();
+console.log(randomJewelNumber1);
+console.log(randomJewelNumber2);
+console.log(randomJewelNumber3);
+console.log(randomJewelNumber4);
+randomTargetNumber = randomTargetNumber(); // this runs the function random() and returns the output of the function as the variable randomTargetNumber
+console.log(randomTargetNumber); //logs the randomTargetNumber
+$("#targetNumber").html(randomTargetNumber);
 
-
-});
+}); 
